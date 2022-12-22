7672c9a37ffef0d1f3f632d4f5adc2b1fb7fed36
Julien Chanaud <chanaud.julien@gmail.com>
2022-12-22 23:16:16 +0000

    Initial commit

---
+++ src/test/java/io/conduktor/metadata/MetadataTest.java
@@ -0,0 +1,21 @@
+package io.conduktor.metadata;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class MetadataTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
